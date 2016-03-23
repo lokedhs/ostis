7bff84904368216e23c372405c95dfac46eb0a24
Elias Martenson <lokedhs@gmail.com>
2016-03-23 12:07:18 +0800

    Enable flycheck and company mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,7 @@
-((c-mode . ((c-basic-offset . 2))))
+((c-mode . ((c-basic-offset . 2)
+            (eval . (progn
+                      (em-append-include-dirs (list (expand-file-name "~/src/ostis")))
+                      (when (eq system-type 'darwin)
+                        (em-append-include-dirs (list "/usr/local/Cellar/gettext/0.18.3.2/include")))
+                      (flycheck-mode 1)
+                      (company-mode 1))))))
